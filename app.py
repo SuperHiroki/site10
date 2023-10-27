10f9aaaa5fef4fa220c81dd583e49ad8f1723ccc
Hiroki <jh147896325jh@gmail.com>
2023-10-27 14:00:44 +0900

    やっぱりログイン必須にした

--- app.py
+++ app.py
@@ -183,9 +183,14 @@ board_first[3,4] = board_first[4,3] = 2  # Black pieces
 @app.route('/', methods=['GET', 'POST'])
 def home():
 
+    '''
     if not current_user.is_authenticated:
         user = User(1, "No Name", "NoNamePassword")
         login_user(user)
+    '''
+
+
+
 
 
     
